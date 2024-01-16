01ee22c16db7d909cfafed22d3586128f444f526
saurabh021120 <64219170+saurabh021120@users.noreply.github.com>
2024-01-17 00:23:53 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,152 @@
-import Image from 'next/image'
+"use client";
+import React, { useState, useRef, useEffect, KeyboardEvent } from 'react';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface Chip {
+  id: number;
+  name: string;
 }
+
+const App: React.FC = () => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [chips, setChips] = useState<Chip[]>([]);
+  const [items, setItems] = useState<string[]>([
+    'Nick Giannopoulos',
+    'John Doe',
+    'Jane Doe',
+    'Alice Smith',
+    'Saurabh Sharma',
+    'Ajay Varma',
+    'HM Shastri',
+    'John Smith',
+    'Alan Done',
+    'Sue Susan'
+  ]);
+  const [filteredItems, setFilteredItems] = useState<string[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [selectedChip, setSelectedChip] = useState<number | null>(null);
+  const [selectedItemIndex, setSelectedItemIndex] = useState<number | null>(null);
+
+  const handleDocumentClick = (event: MouseEvent) => {
+    if (inputRef.current && !inputRef.current.contains(event.target as Node)) {
+      setFilteredItems([]);
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setInputValue(value);
+
+    if (value.trim() === '') {
+      setFilteredItems(items);
+      setSelectedItemIndex(null);
+    }
+
+    const filtered = items.filter(item => item.toLowerCase().includes(value.toLowerCase()));
+    setFilteredItems(filtered);
+    setSelectedItemIndex(filtered.length > 0 ? 0 : null);
+  };
+
+  const handleItemClick = (item: string) => {
+    setChips(prevChips => [...prevChips, { id: Date.now(), name: item }]);
+    setInputValue('');
+    setItems(prevItems => prevItems.filter(prevItem => prevItem !== item));
+    setFilteredItems(prevItems => prevItems.filter(prevItem => prevItem !== item));
+  };
+
+  const handleChipRemove = (chipId: number) => {
+    const removedChip = chips.find(chip => chip.id === chipId);
+    if (removedChip) {
+      setChips(chips.filter(chip => chip.id !== chipId));
+      setItems(prevItems => [...prevItems, removedChip.name]);
+      setFilteredItems(prevItems => [...prevItems, removedChip.name]);
+    }
+    setSelectedChip(null);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'ArrowUp' && selectedChip === null) {
+      event.preventDefault();
+      setSelectedItemIndex(prevIndex => (prevIndex !== null ? Math.max(prevIndex - 1, 0) : null));
+    } else if (event.key === 'ArrowDown' && selectedChip === null) {
+      event.preventDefault();
+      setSelectedItemIndex(prevIndex =>
+        prevIndex !== null ? Math.min(prevIndex + 1, filteredItems.length - 1) : 0
+      );
+    } else if (event.key === 'Enter' && selectedItemIndex !== null) {
+      event.preventDefault();
+      handleItemClick(filteredItems[selectedItemIndex]);
+    }else if (event.key === 'Backspace' && inputValue === '' && chips.length > 0) {
+      const lastChip = chips[chips.length - 1];
+      if (selectedChip === null) {
+        setSelectedChip(lastChip.id);
+      } else {
+        handleChipRemove(lastChip.id);
+      }
+    }
+  }
+
+  const handleOnClick = () => {
+    handleInputChange({ target: { value: '' } } as React.ChangeEvent<HTMLInputElement>)
+  }
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [chips]);
+
+  useEffect(() => {
+    document.addEventListener('click', handleDocumentClick);
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+    };
+  }, []);
+
+
+  return (
+    <div className="container mx-auto my-8 max-w-md">
+      <h1 className='items-center text-center'> ZEPTO</h1>
+      <div className="flex flex-wrap">
+        {chips.map(chip => (
+          <div key={chip.id} className={`${
+            selectedChip === chip.id ? 'bg-black text-white rounded-2xl' : 'bg-gray-500 text-white rounded-2xl'
+          } px-2 py-1 m-1 flex items-center`}>
+            {chip.name}
+            <button
+              className="ml-2"
+              onClick={() => handleChipRemove(chip.id)}
+            >
+              X
+            </button>
+          </div>
+        ))}
+      </div>
+      <input
+        ref={inputRef}
+        type="text"
+        className="border-b border-gray-300 p-2 mt-2 w-full focus:outline-none focus:border-b focus:border-black"
+        placeholder="Add new user..."
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        onClick={handleOnClick}
+      />
+      <div className="mt-2">
+        {filteredItems.map((item,index) => (
+          <div
+            key={item}
+            className={`${
+              selectedItemIndex === index ? 'bg-gray-200' : ''
+            } cursor-pointer p-2 border border-gray-300 mt-1`}
+            onClick={() => handleItemClick(item)}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// @ts-ignore
+export default App;
